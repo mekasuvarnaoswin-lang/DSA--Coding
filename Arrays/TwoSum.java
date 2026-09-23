e541be6b01b40e5ce0f224c1232450e1b6e69370
M. Suvarna Oswin <mekasuvarnaoswin@gmail.com>
2026-09-23 21:32:32 +0530

    Add Two Sum solution
    Implement TwoSum algorithm to find indices of elements that sum to a target.

---
+++ Arrays/TwoSum.java
@@ -0,0 +1,14 @@
+class TwoSum{
+  public static void main(String[] args){
+    int[] a ={1,2,3,4,5};
+    int target = 9;
+    for(int i =0;i<a.length;i++){
+      for(int j =i+1;j<n;j++){
+        if(a[i]+a[j]==target){
+          System.out.print("Target acheived " + i +" " + j);
+        }
+      }
+    }
+  }
+}
+      
